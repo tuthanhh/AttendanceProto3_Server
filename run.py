a3f5e9acf1217917f7775ef7e71f19e5783591e0
TuThanh <tuthanh10825@gmail.com>
2025-08-02 05:32:07 +0700

    fix: create a root route

--- run.py
+++ run.py
@@ -7,6 +7,14 @@ from app.routes import user, student, teacher
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI backend is running"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
 app.include_router(user.router, prefix="/api/users", tags=["Users"])
 app.include_router(teacher.router, prefix="/api/teachers", tags=["Teachers"])
 app.include_router(student.router, prefix="/api/students", tags=["Students"])
